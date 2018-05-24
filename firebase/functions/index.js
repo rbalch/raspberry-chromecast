6e769acb03f88a52a5be9e496993c620eeb169cb
Ryan Balch <rbalch@hugeinc.com>
2018-05-24 17:49:31 -0400

    adding functions for setting database fields for screens

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -2,4 +2,5 @@ const admin = require('firebase-admin');
 admin.initializeApp();
 
 exports.searchGiphy = require('./searchGiphy');
+exports.setScreens = require('./setScreens');
 exports.slackIncomingMessage = require('./slackIncomingMessage');
---
+++ firebase/functions/setScreens.js
@@ -0,0 +1,29 @@
+const admin = require('firebase-admin');
+const functions = require('firebase-functions');
+
+
+module.exports = functions.https.onRequest((req, res) => {
+  const db = admin.firestore();
+  const dataRef = db.collection('huge_cast').doc('cast_1');
+
+  dataRef.get()
+    .then(doc => {
+      return doc.data();
+    })
+    .then(doc => {
+      const query = req.query.query || doc.value;
+      const query_type = req.query.type || 'string';
+      if (query !== doc.value) {
+        doc = {value: query, type: query_type};
+        dataRef.set(doc, {merge: true});
+      }
+      return doc
+    })
+    .then(doc => {
+      return res.status(200).send(doc);
+    })
+    .catch((err) => {
+      console.log(err);
+      return res.status(500).end();
+    });
+});
