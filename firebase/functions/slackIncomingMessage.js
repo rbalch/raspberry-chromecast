7ba97bd522406751d60c5ac7ed6eeb64deb627d1
Zach Goldstein <zgoldstein@google.com>
2018-05-30 19:24:03 -0400

    eslint.

--- firebase/functions/slackIncomingMessage.js
+++ firebase/functions/slackIncomingMessage.js
@@ -50,6 +50,7 @@ function isAuthorized(db, token) {
                 console.warn('Invalid Source.');
                 isAuth = false
             }
+            return null
         })
         .catch(err => {
             console.warn('Error getting document', err);
